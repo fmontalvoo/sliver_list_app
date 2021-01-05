5c59dc46c002ccc958792b4298b661c8ee397492
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-01-05 18:35:46 -0500

    Initial commit.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:sliver_list_app/src/pages/sliver_list_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sliver List App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SliverListPage(),
+    );
+  }
+}
